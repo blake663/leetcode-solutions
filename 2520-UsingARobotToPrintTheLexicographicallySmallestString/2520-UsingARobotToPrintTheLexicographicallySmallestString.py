bcb2ba20c8900628802bd0d1f4e383275bb74051
Blake Celestian <49767763+blake663@users.noreply.github.com>
2025-11-10 20:01:01 -0500

    Create 2520-UsingARobotToPrintTheLexicographicallySmallestString

---
+++ 2520-UsingARobotToPrintTheLexicographicallySmallestString/2520-UsingARobotToPrintTheLexicographicallySmallestString.py
@@ -0,0 +1,15 @@
+# Last updated: 11/10/2025, 8:01:00 PM
+class Solution:
+    def robotWithString(self, s: str) -> str:
+        cnt = Counter(s)
+        stack = []
+        res = []
+        minCharacter = "a"
+        for c in s:
+            stack.append(c)
+            cnt[c] -= 1
+            while minCharacter != "z" and cnt[minCharacter] == 0:
+                minCharacter = chr(ord(minCharacter) + 1)
+            while stack and stack[-1] <= minCharacter:
+                res.append(stack.pop())
+        return "".join(res)
